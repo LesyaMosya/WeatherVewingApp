82af1697461a91034ebef7b955b8d5b15b57e6f2
Olesya <vaska9990@yandex.ru>
2023-07-01 23:19:52 +0300

    Разработка API

--- app/src/main/java/com/example/weathervewingapp/OpenWeatherAPI.java
+++ app/src/main/java/com/example/weathervewingapp/OpenWeatherAPI.java
@@ -1,5 +1,18 @@
 package com.example.weathervewingapp;
 
-public interface OpenWeatherAPI {
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
+public interface OpenWeatherAPI {
+    @GET("/posts/{id}")
+    public Call<Post> getWeather(
+            @Path("id") String id,
+            @Query("lat") float lat,
+            @Query("lon") float lon,
+            @Query("appid") String appid,
+            @Query("lang") String lang,
+            @Query("units") String units
+    );
 }
--- app/src/main/java/com/example/weathervewingapp/Post.java
+++ app/src/main/java/com/example/weathervewingapp/Post.java
@@ -1,5 +1,68 @@
 package com.example.weathervewingapp;
 
-public class Post {
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+public class Post {
+    @SerializedName("temp")
+    @Expose
+    private float temp;
+    @SerializedName("feels_like")
+    @Expose
+    private float feels_like;
+    @SerializedName("humidity")
+    @Expose
+    private int humidity;
+    @SerializedName("speed")
+    @Expose
+    private int speed;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+
+    public float getTemp() {
+        return temp;
+    }
+    public void setTemp(float temp) {
+        this.temp = temp;
+    }
+
+    public float getFeels_like() {
+        return feels_like;
+    }
+    public void setFeels_like(float feels_like) {
+        this.feels_like = feels_like;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
